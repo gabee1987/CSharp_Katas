18f16d00bd767b07b5d9567853a2873c10c21b7c
Gabor Koncz <gabee1987@gmail.com>
2022-05-17 15:18:41 +0200

    Added menu system to the exercise console program

---
+++ Katas/PalindromeCheck.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Katas
+{
+    internal static class PalindromeCheck
+    {
+        // Check if the string is palindrome (it can be read the same forwards as backwards. Ex: “eye”)
+
+    }
+}
--- Katas/Program.cs
+++ Katas/Program.cs
@@ -7,12 +7,118 @@ namespace Katas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine( "Enter a word or sentence" );
-            string stringToManipulate = Console.ReadLine().ToLower();
+            bool IsRunning = true;
+            while ( IsRunning )
+            {
+                bool isValidInput = false;
+                int menuId = 0;
+                while ( !isValidInput )
+                {
+                    PrintMenu();
+                    var input = Console.ReadLine().ToLower();
+                    isValidInput = ErrorHandler( input, out menuId );
+                }
 
-            ReverseStringMethods( stringToManipulate );
-            VowelCountMethods( stringToManipulate );
+                ExecuteChosenExercise( menuId );
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine( "Press ENTER if you would like to try another exercise! \n \n" );
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                if ( keyInfo.Key != ConsoleKey.Enter )
+                    IsRunning = false;
+            }
+            
         }
+
+        #region Menu helper methods
+        private static string ReadStringInputForTheExercise()
+        {
+            Console.WriteLine( "Enter a word or sentence" );
+            return Console.ReadLine().ToLower();
+        }
+
+        private static void ExecuteChosenExercise( int menuId )
+        {
+            string stringToManipulate = string.Empty;
+
+            switch ( menuId )
+            {
+                case 0:
+                    ReverseStringMethods( stringToManipulate );
+                    VowelCountMethods( stringToManipulate );
+                    break;
+                case 1:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine( "Menu #1 selected. Reverse a string \n" );
+                    Console.ForegroundColor = ConsoleColor.White;
+                    stringToManipulate = ReadStringInputForTheExercise();
+                    ReverseStringMethods( stringToManipulate );
+                    break;
+                case 2:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine( "Menu #2 selected. Count the vowels \n" );
+                    Console.ForegroundColor = ConsoleColor.White;
+                    stringToManipulate = ReadStringInputForTheExercise();
+                    VowelCountMethods( stringToManipulate );
+                    break;
+                case 3:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine( "Menu #3 selected. Check if the string is a palindrome (it can be read the same forwards as backwards. Ex: “eye”) \n" );
+                    Console.ForegroundColor = ConsoleColor.White;
+                    stringToManipulate = ReadStringInputForTheExercise();
+                    VowelCountMethods( stringToManipulate );
+                    break;
+                default:
+                    ReverseStringMethods( stringToManipulate );
+                    VowelCountMethods( stringToManipulate );
+                    break;
+            }
+        }
+
+        private static void PrintMenu()
+        {
+            Console.WriteLine( "Choose which string exercise would you like to try by pressing the menu number \n" );
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write( "#1. " );
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write( "Reverse a string \n" );
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write( "#2. " );
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write( "Count the vowels in a string \n" );
+
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write( "#3. " );
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write( "Check if the string is a palindrome (it can be read the same forwards as backwards. Ex: “eye”) \n" );
+        }
+
+        private static bool ErrorHandler( string input, out int menuId )
+        {
+            if ( !int.TryParse( input, out menuId ) )
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine( "Wrong input! You must select a number from the menu!" );
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine();
+                return false;
+            }
+            int menuCount = 3;
+            if ( menuId > menuCount )
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine( "Wrong input! You must select a number from the menu numbers!" );
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine();
+                return false;
+            }
+
+            Console.WriteLine();
+            return true;
+        }
+        #endregion
 
         private static void ReverseStringMethods( string stringToManipulate )
         {
