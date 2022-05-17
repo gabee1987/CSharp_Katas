a277f9252c35eae3f945a1781b18802e9f4bf715
Gabor Koncz <gabee1987@gmail.com>
2022-05-17 12:20:11 +0200

    Changed the exercises to dynamic input string

--- Katas/Program.cs
+++ Katas/Program.cs
@@ -7,28 +7,29 @@ namespace Katas
     {
         static void Main(string[] args)
         {
-            ReverseStringMethods();
+            Console.WriteLine( "Enter a word or sentence" );
+            string stringToManipulate = Console.ReadLine().ToLower();
 
-            VowelCountMethods();
+            ReverseStringMethods( stringToManipulate );
+            VowelCountMethods( stringToManipulate );
         }
 
-        private static void ReverseStringMethods()
+        private static void ReverseStringMethods( string stringToManipulate )
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine( "====================== Reverse a string" );
             Console.ForegroundColor = ConsoleColor.White;
 
-            StringReverse.ReverseString_1( "Hello" );
-            StringReverse.ReverseString_2( "Hello" );
-            StringReverse.ReverseString_3( "Hello" );
-            StringReverse.ReverseString_4( "Hello" );
+            StringReverse.ReverseString_1( stringToManipulate );
+            StringReverse.ReverseString_2( stringToManipulate );
+            StringReverse.ReverseString_3( stringToManipulate );
+            StringReverse.ReverseString_4( stringToManipulate );
 
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine( "#5 Solution (Extension Method)" );
             Console.ForegroundColor = ConsoleColor.White;
-            var stringToReverse = "Hello";
-            var result5 = stringToReverse.ReverseString_5();
-            Console.WriteLine( $"Original string -> {stringToReverse}" );
+            var result5 = stringToManipulate.ReverseString_5();
+            Console.WriteLine( $"Original string -> {stringToManipulate}" );
             Console.Write( "Reversed string -> " );
             Console.ForegroundColor = ConsoleColor.Green;
             Console.Write( result5 + "\n" );
@@ -37,15 +38,15 @@ namespace Katas
             Console.WriteLine( "\n" );
         }
 
-        private static void VowelCountMethods()
+        private static void VowelCountMethods( string stringToManipulate )
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine( "====================== Count the vowels in a string" );
             Console.ForegroundColor = ConsoleColor.White;
 
-            VowelsCount.VowelCounter_1( "Awesome" );
-            VowelsCount.VowelCounter_2( "Awesome" );
-            VowelsCount.VowelCounter_3( "Awesome" );
+            VowelsCount.VowelCounter_1( stringToManipulate );
+            VowelsCount.VowelCounter_2( stringToManipulate );
+            VowelsCount.VowelCounter_3( stringToManipulate );
 
             Console.WriteLine( "\n" );
         }
